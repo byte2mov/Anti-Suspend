556a986d686d549066e9214affa24cd4ae184cf0
byte2mov <146471523+byte2mov@users.noreply.github.com>
2024-04-01 04:25:59 -0400

    Add files via upload

---
+++ bypass_process_freeze/bypass_process_freeze.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+
+class c_globals {
+public:
+    HANDLE hThread; // thread handle we create.
+    int count = 128; // amount of times we suspend
+
+    // class to store all out information.
+}; static c_globals* globals = new c_globals();
+
+int main() {
+    
+    globals->hThread = CreateThread(NULL, 0, 0, NULL, 0, NULL); // dummy thread to base detections from.
+
+    DebugActiveProcessStop(GetCurrentProcessId()); // self explanatory 
+
+    for (size_t i = 0; i < globals->count; ++i) {
+        SuspendThread(globals->hThread); // max out the limit.
+    }
+
+    while (globals->hThread != reinterpret_cast<HANDLE>((DWORD)-1))
+    {
+        if (SuspendThread(globals->hThread) != (DWORD)-1) {
+            std::cout << "Anti Suspend Activated. \n" << std::endl;
+            std::cin.get(); // here i chose to just stop everything, in a real case scenario you would send a notifaction or kill the program.
+        }
+        std::cout << "we are good. \n"; // here you do nothing. || real case scenario this should be threaded.
+    }
+
+    CloseHandle(globals->hThread);
+}
